321a7a08c36b8ac0a08e238ecd648a627d1e2e42
Kingshuk <afkingshuk@gmail.com>
2022-07-26 04:14:42 +0600

    Final pre release V1.1

---
+++ afksoundm/include/demo_mqtt.h
@@ -0,0 +1,35 @@
+#ifndef __DEMO_MQTT_H_
+#define __DEMO_MQTT_H_
+
+
+// #define BROKER_IP  "mqtt.neucrack.com"
+// #define BROKER_PORT 1883
+// #define CLIENT_USER "mqtt"
+// #define CLIENT_PASS "mqtt"
+// #define SUBSCRIBE_TOPIC "$neucrack/app" 
+// #define PUBLISH_TOPIC   "$neucrack/gprs"
+// #define PUBLISH_INTERVAL 10000 //10s
+// #define PUBLISH_PAYLOEAD "hello I'm from gprs module"
+
+#define BROKER_IP  "broker.hivemq.com"
+#define BROKER_PORT 1883
+#define BROKER_HOSTNAME "broker.hivemq.com"
+#define CLIENT_USER "NULL"
+#define CLIENT_PASS "NULL"
+#define SUBSCRIBE_TOPIC "DMA/afkingshuk/SOUND"
+#define PUBLISH_TOPIC   "DMA/afkingshuk/SOUND"
+#define PUBLISH_INTERVAL 2500 //10s //1s //2.5s
+#define PUBLISH_PAYLOAD "hello I'm from gprs module for test"
+
+// #define BROKER_IP  "broker.hivemq.com"
+// #define BROKER_PORT 1883
+// #define CLIENT_USER "mqtt"
+// #define CLIENT_PASS ""
+// #define SUBSCRIBE_TOPIC "$DMA/A9G"
+// #define PUBLISH_TOPIC   "$DMA/A9G"
+// #define PUBLISH_INTERVAL 10000 //10s
+// #define PUBLISH_PAYLOEAD "hello I'm from gprs module"
+
+
+#endif
+
